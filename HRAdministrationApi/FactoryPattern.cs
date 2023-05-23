e9cb8351092c1739298f8d58c14968ffd708791d
clausaji <c.sanchezjimenez@qquest.nl>
2023-05-23 10:03:52 +0200

    all the tutorial

---
+++ HRAdministrationApi/FactoryPattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRAdministrationApi
+{
+    //The method instantiate an object generically.
+    /*The where T : class, K, new() constraint is applied to the T type parameter. 
+     * This constraint ensures that T must be a reference type (class), 
+     * implement or derive from the K base type, and have a default parameterless constructor (new()).
+    */
+    public static class FactoryPattern<K,T> where T : class,K,new()
+    {
+        public static K GetInstance()
+        {
+            K objK;
+            objK = new T();
+            return objK;
+
+        }
+    }
+}
---
+++ SchoolAdministrationTutorial/Program.cs
@@ -0,0 +1,123 @@
+﻿using HRAdministrationApi;
+using static SchoolAdministrationTutorial.Program;
+
+namespace SchoolAdministrationTutorial
+{
+    public enum EmployeeType
+    {
+        Teacher,
+        HeadOfDepartment,
+        DeputyHeadMaster,
+        HeadMaster
+    }
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            decimal totalSalaries = 0;
+            List<IEmployee> employees = new List<IEmployee>();
+
+            SeedData(employees);
+
+            /*    All this will be now replaced by a shorter version
+             *    foreach (IEmployee employee in employees)
+                 {
+                     totalSalaries += employee.Salary;*/
+        
+            //This .Sum is a LINQ functionality
+            Console.WriteLine($"Total Annual Salaries (including bonus): {employees.Sum(e => e.Salary)}");
+
+            Console.ReadKey();
+
+        }
+
+        //This method populates the data
+        public static void SeedData(List<IEmployee> employees)
+        {
+            IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Bob", "Fisher", 40000);
+            employees.Add(teacher1);
+            IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Clau", "Saji", 40000);
+            employees.Add(teacher2);
+            
+            IEmployee headOfDepartment = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 3, "Mike", "Smit", 50000);
+            employees.Add(headOfDepartment);
+
+            IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.DeputyHeadMaster, 4, "Ainhoa", "Carrillo", 60000);
+            employees.Add(deputyHeadMaster);
+
+            IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadMaster, 5, "Ruud", "Sanchez", 80000);
+            employees.Add(headMaster);
+        }
+    }
+
+    public class Teacher : EmployeeBase
+    {
+        //base.Salary refers to the salary property of the base class
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); }
+    }
+
+    public class HeadOfDepartment : EmployeeBase
+    {
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.03m); }
+
+    }
+    public class DeputyHeadMaster : EmployeeBase
+    {
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.04m); }
+
+    }
+
+    public class HeadMaster : EmployeeBase
+    {
+        public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
+
+    }
+
+    //This is the facotry design pattern
+    //It is static because we don't need to instantiate it.
+    public static class EmployeeFactory
+    {
+        public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
+        {
+            IEmployee employee = null;
+
+            switch (employeeType)
+            {
+                case EmployeeType.Teacher:
+                    employee = FactoryPattern<IEmployee, Teacher>.GetInstance();
+                    break;
+                case EmployeeType.HeadOfDepartment:
+                    employee = FactoryPattern<IEmployee, HeadOfDepartment>.GetInstance();
+
+                    break;
+                case EmployeeType.DeputyHeadMaster:
+                    employee = FactoryPattern<IEmployee, DeputyHeadMaster>.GetInstance();
+                    break;
+                case EmployeeType.HeadMaster:
+                    employee = FactoryPattern<IEmployee, HeadMaster>.GetInstance();
+                    break;
+                default:
+                    break;
+
+            }
+            if (employee != null)
+            {
+                employee.Id = id;
+                employee.FirstName = firstName;
+                employee.LastName = lastName;
+                employee.Salary = salary;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+
+            return employee;
+        }
+    }
+
+
+
+}
